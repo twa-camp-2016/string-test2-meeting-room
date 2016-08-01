b7eff50c7dbcf81c4bbb117948a09821ad7cb774
Dann <dann_summer@163.com>
2016-08-01 11:43:53 +0800

    李丹

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -271,7 +271,7 @@ describe('Take out food', function () {
     expect(summary).toEqual(expected)
   });
 
-  fit('should generate best charge when no promotion can be used', function() {
+  it('should generate best charge when no promotion can be used', function() {
     let inputs = ["ITEM0013 x 4"];
     let summary = bestCharge(inputs).trim();
     let expected = `
